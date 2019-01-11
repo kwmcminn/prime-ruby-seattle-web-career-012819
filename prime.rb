8c3b852aad90397a2e4d1009efff464270f4fde4
kwmcminn <kwmcminn@gmail.com>
2019-01-11 13:16:40 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime? (n)
+  if n < 2
+    return false
+  elsif n == 3
+    return true
+  end
+  array = []
+  for i in (2..(n/2)) do
+    array.push(i)
+  end
+  array.each do |i|
+    if n % i == 0
+      return false
+    end
+  end
+  return true
+end
